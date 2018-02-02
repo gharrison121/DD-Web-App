0c91b38063f0a8c40a659a9f91a86dd04971ac32
gharrison121 <gharrison121@gmail.com>
2018-02-02 14:11:45 +0000

    add row changes
    add row functionality now more modular

--- app/createdd.js
+++ app/createdd.js
@@ -1,22 +1,27 @@
 console.log("Create DD Page")
 
 //ID Getters
-var addrow = document.getElementById("addrow")
-var ddtable = document.getElementById("datadictionary")
+// var addrow = document.getElementById("addrow")
+// var ddtable = document.getElementById("datadictionary")
 //Event Listeners
-addrow.addEventListener("click", addRow)
+// addrow.addEventListener("click", addRow)
 
-function addRow() {
+//Handles which add row button has been clicked
+function idHandler() {
+  addRow(event.srcElement.id)
+}
+
+function addRow(id) {
   console.log("addrow function")
+  var rowBtn = document.getElementById(id)
+  currentTable = rowBtn.closest("tbody")
   var newRow = document.createElement("tr")
-  //Loop through each column in the table
+  //Loop through each column in the table and create cell
   for (var i=0; i < 8; i++) {
-    console.log(i)
     var cell = document.createElement("td")
 
     //Columns have different input types
     if (i == 1) {
-      console.log("if statement true")
       var validInputs = ['Primary Key', 'Foreign Key', 'Candidate Key']
       var select = document.createElement("select")
       for (var j in validInputs) {
@@ -30,7 +35,7 @@ function addRow() {
     } else if (i == 2) {
       var validInputs = ['Int', 'VARCHAR', 'More']
       var select = document.createElement("select")
-
+      //Loop through the validInputs and create an option for each
       for (var j in validInputs) {
         var option = document.createElement("option")
         option.value = validInputs[j]
@@ -51,6 +56,11 @@ function addRow() {
       newRow.appendChild(cell)
     }
   }
-  console.log("end of function")
-  ddtable.append(newRow)
+  //insert new row before the add row button
+  currentTable.insertBefore(newRow, currentTable.lastElementChild)
+
+}
+
+function addTable() {
+
 }
